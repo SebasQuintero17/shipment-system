89c87c641af6f247f149276833fb66e562f141c8
Sebastian Quintero <SebasQuintero345@hotmail.com>
2026-03-03 19:47:52 -0500

    fix ci

--- app/database.py
+++ app/database.py
@@ -2,12 +2,17 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
+
+
 DATABASE_URL = os.getenv(
-	"DATABASE_URL",
-	"postgresql://admin:admin@localhost:5433/shipments"
+    "DATABASE_URL",
+    "sqlite:///./test.db"
 )
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False} if "sqlite" in DATABASE_URL else {}
+)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
